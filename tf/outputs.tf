c4daf8624de07dc921f5a7ce8e7a8e035accf88f
Diego Arce <Diego@arce.cr>
2021-07-28 19:40:07 -0600

    feat: enhances to terraform outputs
    1. Add aws_instance_public_endpoint
    2. Change rds outputs as object
    
    Signed-off-by: Diego Arce <Diego@arce.cr>

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,17 +1,19 @@
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.default.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.default.port
-  sensitive   = true
-}
-
-output "rds_username" {
+output "postgres_instance" {
   description = "RDS instance root username"
-  value       = aws_db_instance.default.username
+  value       = {
+    endpoint = "${aws_db_instance.default.address}:${aws_db_instance.default.port}"
+    hostname = aws_db_instance.default.address
+    port = aws_db_instance.default.port
+    rds_username = aws_db_instance.default.username
+  }
+}
+
+output "rds_password" {
+  description = "RDS instance root password"
+  value       = aws_db_instance.default.password
   sensitive   = true
+}
+
+output "aws_instance_public_endpoint" {
+  value = "http://${aws_eip.web.public_dns}"
 }
